25b70503194cc9a79775557aad6ea95aad4e4d25
frotaluiza <46004949+frotaluiza@users.noreply.github.com>
2021-09-15 10:47:09 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-// module.exports = {
-//     devServer: {
-//         proxy: 'https://pessoas120210914201201-2.azurewebsites.net'
-//       }
-// }
+module.exports = {
+    devServer: {
+        proxy: 'https://pessoas120210914201201-2.azurewebsites.net'
+      }
+}
