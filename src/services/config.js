5867aa70ea32c13204b9cdf1ef93a0150652da89
frotaluiza <46004949+frotaluiza@users.noreply.github.com>
2021-09-15 10:58:42 -0300

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: 'https://green-wave-0ab9cb110.azurestaticapps.net/api/'
+    baseURL: 'https://pessoas120210914201201-2.azurewebsites.net/api/'
 })
